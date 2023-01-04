5a0531f7a43ab8322d61cc5339c4e4e57f539506
Kmek <kmekeefe@gmail.com>
2023-01-04 11:04:15 -0500

    Give cup back after drinking milk tea

--- src/main/java/com/kmek/bobamod/food/MilkTeaItem.java
+++ src/main/java/com/kmek/bobamod/food/MilkTeaItem.java
@@ -1,11 +1,15 @@
 package com.kmek.bobamod.food;
 
+import com.kmek.bobamod.init.ItemInit;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
 
 public class MilkTeaItem extends Item {
@@ -17,7 +21,15 @@ public class MilkTeaItem extends Item {
                         .build()));
     }
 
-    // todo return cup after drinking
+    // Eat item and then return cup
+    @Override
+    public ItemStack finishUsingItem(ItemStack p_42923_, Level p_42924_, LivingEntity p_42925_) {
+        if (p_42925_ instanceof Player player) {
+            player.addItem(new ItemStack(ItemInit.MILK_TEA_CUP.get()));
+        }
+
+        return p_42925_.eat(p_42924_, p_42923_);
+    }
 
     // todo future idea: cure all harmful potions the same way milk does
 
