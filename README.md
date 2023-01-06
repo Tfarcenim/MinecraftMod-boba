4c4a0a59a8b809d56165d9e7476280f614977836
Kmek <kmekeefe@gmail.com>
2023-01-06 17:33:42 -0500

    Update readme, update mod image, rebalance milk tea values

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 # BobaMod
 ![bobamod image](./src/main/resources/bobamod.png)
 
-## Bubble Milk Teas
-### Very Sweet
+---
+# Items
+
+### Crop
+- ![](./src/main/resources/assets/bobamod/textures/item/cassava_cutting.png) Cassava Cutting
+
+### Crafting Ingredients
+- ![](./src/main/resources/assets/bobamod/textures/item/cassava.png) Cassava
+- ![](./src/main/resources/assets/bobamod/textures/item/tapioca_balls.png) Boba Pearls
+- ![](./src/main/resources/assets/bobamod/textures/item/milk_tea_cup.png) Milk Tea Cup
+- ![](./src/main/resources/assets/bobamod/textures/item/taiyaki_mold.png) Taiyaki Mold
+
+### Bubble Milk Teas
+#### Very Sweet
 - ![](./src/main/resources/assets/bobamod/textures/item/brown_sugar_milk_tea.png) Brown Sugar Milk Tea
 - ![](./src/main/resources/assets/bobamod/textures/item/tiger_milk_tea.png) Tiger Milk Tea
 - ![](./src/main/resources/assets/bobamod/textures/item/honey_milk_tea.png) Honey Milk Tea
@@ -12,19 +24,29 @@
 - ![](./src/main/resources/assets/bobamod/textures/item/cookie_milk_tea.png) Cookie Milk Tea
 - ![](./src/main/resources/assets/bobamod/textures/item/cake_milk_tea.png) Cake Milk Tea
 - ![](./src/main/resources/assets/bobamod/textures/item/pumpkin_spice_milk_tea.png) Pumpkin Spice Milk Tea
-### Fruit
+#### Fruit
 - ![](./src/main/resources/assets/bobamod/textures/item/apple_milk_tea.png) Apple Milk Tea
 - ![](./src/main/resources/assets/bobamod/textures/item/sweet_berry_milk_tea.png) Sweet Berry Milk Tea
 - ![](./src/main/resources/assets/bobamod/textures/item/watermelon_milk_tea.png) Watermelon Milk Tea
 - ![](./src/main/resources/assets/bobamod/textures/item/glow_berry_milk_tea.png) Glow Berry Milk Tea
 - ![](./src/main/resources/assets/bobamod/textures/item/chorus_fruit_milk_tea.png) Chorus Fruit Milk Tea
-### Floral
+#### Floral
 - ![](./src/main/resources/assets/bobamod/textures/item/rose_milk_tea.png) Rose Milk Tea
 - ![](./src/main/resources/assets/bobamod/textures/item/butterfly_pea_flower_milk_tea.png) Butterfly Pea Flower Milk Tea
 - ![](./src/main/resources/assets/bobamod/textures/item/matcha_milk_tea.png) Matcha Milk Tea
 - ![](./src/main/resources/assets/bobamod/textures/item/lavender_milk_tea.png) Lavender Milk Tea
-### Misc.
+#### Misc.
 - ![](./src/main/resources/assets/bobamod/textures/item/thai_milk_tea.png) Thai Milk Tea
 - ![](./src/main/resources/assets/bobamod/textures/item/beetroot_milk_tea.png) Beetroot Milk Tea
 - ![](./src/main/resources/assets/bobamod/textures/item/carrot_milk_tea.png) Carrot Milk Tea
 - ![](./src/main/resources/assets/bobamod/textures/item/kelp_milk_tea.png) Kelp Milk Tea
+
+### Food
+- ![](./src/main/resources/assets/bobamod/textures/item/taiyaki.png) Taiyaki
+
+### Paintings
+- ![](./src/main/resources/assets/bobamod/textures/painting/taiyaki.png)
+- ![](./src/main/resources/assets/bobamod/textures/painting/taiyaki_mold.png)
+- ![](./src/main/resources/assets/bobamod/textures/painting/boba_sign.png)
+- ![](./src/main/resources/assets/bobamod/textures/painting/brown_sugar_milk_tea.png)
+- ![](./src/main/resources/assets/bobamod/textures/painting/milk_teas_trio.png)
--- src/main/java/com/kmek/bobamod/item/ModItemInit.java
+++ src/main/java/com/kmek/bobamod/item/ModItemInit.java
@@ -25,7 +25,9 @@ public class ModItemInit {
             () -> new ItemNameBlockItem(ModBlocksInit.CASSAVA_CROP.get(), new Item.Properties()));
     public static final RegistryObject<Item> CASSAVA = ITEMS.register("cassava", () -> new Item(new Item.Properties().food(
             new FoodProperties.Builder().nutrition(0).saturationMod(0.1f)
-                    .effect(() -> new MobEffectInstance(MobEffects.POISON, 200, 0), 1.0f).build())));
+                    .effect(() -> new MobEffectInstance(MobEffects.POISON, 200, 0), 1.0f)
+                    .effect(() -> new MobEffectInstance(MobEffects.WEAKNESS, 100, 0), 1.0f)
+                    .effect(() -> new MobEffectInstance(MobEffects.HUNGER, 200, 0), 1.0f).build())));
 
     /**
      * Crafting Ingredients
@@ -38,37 +40,59 @@ public class ModItemInit {
      * Food Snacks
      */
     public static final RegistryObject<Item> TAIYAKI = ITEMS.register("taiyaki", () -> new Item(new Item.Properties().food(
-            new FoodProperties.Builder().nutrition(2).saturationMod(0.5f).build())));
+            new FoodProperties.Builder().nutrition(2).saturationMod(0.8f).build())));
 
     /**
      * Bubble Milk Teas
      */
     // Very sweet
-    public static final RegistryObject<Item> BROWN_SUGAR_MILK_TEA = ITEMS.register("brown_sugar_milk_tea", () -> new MilkTeaItem(2, 0.5f));
-    public static final RegistryObject<Item> TIGER_MILK_TEA = ITEMS.register("tiger_milk_tea", () -> new MilkTeaItem(2, 0.5f));
-    public static final RegistryObject<Item> HONEY_MILK_TEA = ITEMS.register("honey_milk_tea", () -> new MilkTeaItem(2, 0.5f));
-    public static final RegistryObject<Item> CHOCOLATE_MILK_TEA = ITEMS.register("chocolate_milk_tea", () -> new MilkTeaItem(2, 0.5f));
-    public static final RegistryObject<Item> VANILLA_MILK_TEA = ITEMS.register("vanilla_milk_tea", () -> new MilkTeaItem(2, 0.5f));
-    public static final RegistryObject<Item> EGG_CUSTARD_MILK_TEA = ITEMS.register("egg_custard_milk_tea", () -> new MilkTeaItem(2, 0.5f));
-    public static final RegistryObject<Item> COOKIE_MILK_TEA = ITEMS.register("cookie_milk_tea", () -> new MilkTeaItem(2, 0.5f));
-    public static final RegistryObject<Item> CAKE_MILK_TEA = ITEMS.register("cake_milk_tea", () -> new MilkTeaItem(2, 0.5f));
-    public static final RegistryObject<Item> PUMPKIN_SPICE_MILK_TEA = ITEMS.register("pumpkin_spice_milk_tea", () -> new MilkTeaItem(4, 2f));
+    public static final RegistryObject<Item> BROWN_SUGAR_MILK_TEA = ITEMS.register("brown_sugar_milk_tea",
+            () -> new MilkTeaItem(2, 0.5f));
+    public static final RegistryObject<Item> TIGER_MILK_TEA = ITEMS.register("tiger_milk_tea",
+            () -> new MilkTeaItem(2, 0.5f));
+    public static final RegistryObject<Item> HONEY_MILK_TEA = ITEMS.register("honey_milk_tea",
+            () -> new MilkTeaItem(6, 0.5f));
+    public static final RegistryObject<Item> CHOCOLATE_MILK_TEA = ITEMS.register("chocolate_milk_tea",
+            () -> new MilkTeaItem(2, 0.6f));
+    public static final RegistryObject<Item> VANILLA_MILK_TEA = ITEMS.register("vanilla_milk_tea",
+            () -> new MilkTeaItem(2, 0.5f));
+    public static final RegistryObject<Item> EGG_CUSTARD_MILK_TEA = ITEMS.register("egg_custard_milk_tea",
+            () -> new MilkTeaItem(2, 0.5f));
+    public static final RegistryObject<Item> COOKIE_MILK_TEA = ITEMS.register("cookie_milk_tea",
+            () -> new MilkTeaItem(2, 0.5f));
+    public static final RegistryObject<Item> CAKE_MILK_TEA = ITEMS.register("cake_milk_tea",
+            () -> new MilkTeaItem(15, 2.9f));
+    public static final RegistryObject<Item> PUMPKIN_SPICE_MILK_TEA = ITEMS.register("pumpkin_spice_milk_tea",
+            () -> new MilkTeaItem(2, 0.6f));
     // Fruit teas
-    public static final RegistryObject<Item> APPLE_MILK_TEA = ITEMS.register("apple_milk_tea", () -> new MilkTeaItem(4, 2f));
-    public static final RegistryObject<Item> SWEET_BERRY_MILK_TEA = ITEMS.register("sweet_berry_milk_tea", () -> new MilkTeaItem(2, 0.5f));
-    public static final RegistryObject<Item> WATERMELON_MILK_TEA = ITEMS.register("watermelon_milk_tea", () -> new MilkTeaItem(4, 2f));
-    public static final RegistryObject<Item> GLOW_BERRY_MILK_TEA = ITEMS.register("glow_berry_milk_tea", () -> new MilkTeaItem(2, 0.5f));
-    public static final RegistryObject<Item> CHORUS_FRUIT_MILK_TEA = ITEMS.register("chorus_fruit_milk_tea", () -> new MilkTeaItem(4, 2f));
+    public static final RegistryObject<Item> APPLE_MILK_TEA = ITEMS.register("apple_milk_tea",
+            () -> new MilkTeaItem(4, 2.5f));
+    public static final RegistryObject<Item> SWEET_BERRY_MILK_TEA = ITEMS.register("sweet_berry_milk_tea",
+            () -> new MilkTeaItem(2, 0.5f));
+    public static final RegistryObject<Item> WATERMELON_MILK_TEA = ITEMS.register("watermelon_milk_tea",
+            () -> new MilkTeaItem(2, 1.5f));
+    public static final RegistryObject<Item> GLOW_BERRY_MILK_TEA = ITEMS.register("glow_berry_milk_tea",
+            () -> new MilkTeaItem(2, 0.5f));
+    public static final RegistryObject<Item> CHORUS_FRUIT_MILK_TEA = ITEMS.register("chorus_fruit_milk_tea",
+            () -> new MilkTeaItem(4, 2.5f));
     // Floral
-    public static final RegistryObject<Item> ROSE_MILK_TEA = ITEMS.register("rose_milk_tea", () -> new MilkTeaItem(4, 2f));
-    public static final RegistryObject<Item> BUTTERFLY_PEA_FLOWER_MILK_TEA = ITEMS.register("butterfly_pea_flower_milk_tea", () -> new MilkTeaItem(4, 2f));
-    public static final RegistryObject<Item> MATCHA_MILK_TEA = ITEMS.register("matcha_milk_tea", () -> new MilkTeaItem(2, 0.5f));
-    public static final RegistryObject<Item> LAVENDER_MILK_TEA = ITEMS.register("lavender_milk_tea", () -> new MilkTeaItem(2, 0.5f));
+    public static final RegistryObject<Item> ROSE_MILK_TEA = ITEMS.register("rose_milk_tea",
+            () -> new MilkTeaItem(3, 0.6f));
+    public static final RegistryObject<Item> BUTTERFLY_PEA_FLOWER_MILK_TEA = ITEMS.register("butterfly_pea_flower_milk_tea",
+            () -> new MilkTeaItem(3, 0.6f));
+    public static final RegistryObject<Item> MATCHA_MILK_TEA = ITEMS.register("matcha_milk_tea",
+            () -> new MilkTeaItem(3, 1f));
+    public static final RegistryObject<Item> LAVENDER_MILK_TEA = ITEMS.register("lavender_milk_tea",
+            () -> new MilkTeaItem(3, 0.6f));
     // Misc
-    public static final RegistryObject<Item> THAI_MILK_TEA = ITEMS.register("thai_milk_tea", () -> new MilkTeaItem(4, 2f));
-    public static final RegistryObject<Item> BEETROOT_MILK_TEA = ITEMS.register("beetroot_milk_tea", () -> new MilkTeaItem(3, 2f));
-    public static final RegistryObject<Item> CARROT_MILK_TEA = ITEMS.register("carrot_milk_tea", () -> new MilkTeaItem(4, 2f));
-    public static final RegistryObject<Item> KELP_MILK_TEA = ITEMS.register("kelp_milk_tea", () -> new MilkTeaItem(4, 2f));
+    public static final RegistryObject<Item> THAI_MILK_TEA = ITEMS.register("thai_milk_tea",
+            () -> new MilkTeaItem(2, 0.6f));
+    public static final RegistryObject<Item> BEETROOT_MILK_TEA = ITEMS.register("beetroot_milk_tea",
+            () -> new MilkTeaItem(1, 1.5f));
+    public static final RegistryObject<Item> CARROT_MILK_TEA = ITEMS.register("carrot_milk_tea",
+            () -> new MilkTeaItem(4, 3.8f));
+    public static final RegistryObject<Item> KELP_MILK_TEA = ITEMS.register("kelp_milk_tea",
+            () -> new MilkTeaItem(1, 1f));
 
     /**
      * Registering the event bus
