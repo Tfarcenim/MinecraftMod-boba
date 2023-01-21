7efdf6f57ae453ae65a0a75aff3a569f8b59f046
Kmek <kmekeefe@gmail.com>
2023-01-20 20:55:56 -0500

    Organize MineCafe creative mode tab in a separate file

--- src/main/java/com/kmek/minecafe/MineCafeMod.java
+++ src/main/java/com/kmek/minecafe/MineCafeMod.java
@@ -4,6 +4,7 @@ import com.kmek.minecafe.block.ModBlocksInit;
 import com.kmek.minecafe.block.entity.ModBlockEntities;
 import com.kmek.minecafe.fluid.ModFluidTypes;
 import com.kmek.minecafe.fluid.ModFluids;
+import com.kmek.minecafe.item.ModCreativeTabInit;
 import com.kmek.minecafe.item.ModItemsInit;
 import com.kmek.minecafe.loot.ModLootModifiers;
 import com.kmek.minecafe.networking.ModMessages;
@@ -15,17 +16,12 @@ import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.core.Direction;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.ComposterBlock;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -43,8 +39,6 @@ public class MineCafeMod {
     public static final String MODID = "minecafe";
     // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
-
-    CreativeModeTab bobaTab;
 
     public MineCafeMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -68,9 +62,9 @@ public class MineCafeMod {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
-        // Register the item to a creative tab
-        modEventBus.addListener(this::addCreative);
-        modEventBus.addListener(this::registerTabs);
+        // Register creative mode tabs
+        ModCreativeTabInit creativeTabsInit = new ModCreativeTabInit();
+        creativeTabsInit.register(modEventBus);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
@@ -82,255 +76,6 @@ public class MineCafeMod {
 
         ComposterBlock.COMPOSTABLES.put(ModItemsInit.CASSAVA.get(), 0.65F);
         ComposterBlock.COMPOSTABLES.put(ModItemsInit.COFFEE_FILTER_USED.get(), 0.65F);
-    }
-
-    // todo bug: this tab has no title
-    private void registerTabs(CreativeModeTabEvent.Register event) {
-         bobaTab = event.registerCreativeModeTab(new ResourceLocation(MODID, "minecafe"), builder -> builder
-                .icon(() -> new ItemStack(ModItemsInit.BROWN_SUGAR_MILK_TEA.get()))
-                .displayItems((featureFlags, output, hasOp) -> {
-                    // Blocks
-                    output.accept(ModItemsInit.CAKE_STAND_ITEM.get());
-                    output.accept(ModItemsInit.DISPLAY_CASE_CURVED_ITEM.get());
-                    output.accept(ModItemsInit.OAK_WALL_SHELF_ITEM.get());
-                    output.accept(ModItemsInit.WAFFLE_IRON_ITEM.get());
-                    output.accept(ModItemsInit.COFFEE_MACHINE_ITEM.get());
-                    output.accept(ModItemsInit.ESPRESSO_MACHINE_ITEM.get());
-
-                    // Fun Blocks
-                    output.accept(ModItemsInit.COFFEE_BUCKET.get());
-
-                    // Crops & seeds
-                    output.accept(ModItemsInit.CASSAVA_CUTTING.get());
-                    output.accept(ModItemsInit.CASSAVA.get());
-                    output.accept(ModItemsInit.COFFEE_CHERRIES.get());
-
-                    // Jams
-                    output.accept(ModItemsInit.APPLE_JAM.get());
-                    output.accept(ModItemsInit.CHORUS_FRUIT_JAM.get());
-                    output.accept(ModItemsInit.GLOW_BERRY_JAM.get());
-                    output.accept(ModItemsInit.SWEET_BERRY_JAM.get());
-                    output.accept(ModItemsInit.WATERMELON_JAM.get());
-
-                    // Creams
-                    output.accept(ModItemsInit.CREAM.get());
-                    output.accept(ModItemsInit.WHIPPED_CREAM.get());
-                    output.accept(ModItemsInit.CHOCOLATE_CREAM.get());
-                    output.accept(ModItemsInit.VANILLA_CREAM.get());
-                    output.accept(ModItemsInit.MATCHA_CREAM.get());
-                    output.accept(ModItemsInit.HONEY_CREAM.get());
-                    output.accept(ModItemsInit.ROSE_CREAM.get());
-                    output.accept(ModItemsInit.MARSHMALLOW.get());
-                    output.accept(ModItemsInit.MARSHMALLOWS.get());
-                    output.accept(ModItemsInit.CUSTARD.get());
-                    output.accept(ModItemsInit.BUTTER.get());
-//                    output.accept(ModItemsInit.PASTRY_BAG.get());
-
-                    output.accept(ModItemsInit.CHOCOLATE.get());
-
-                    // Coffee Crafting Items
-                    output.accept(ModItemsInit.COFFEE_BEANS_UNROASTED.get());
-                    output.accept(ModItemsInit.COFFEE_BEANS_ROASTED.get());
-                    output.accept(ModItemsInit.COFFEE_GROUNDS.get());
-                    output.accept(ModItemsInit.COFFEE_POT_FULL.get());
-                    output.accept(ModItemsInit.COFFEE_FILTER.get());
-                    output.accept(ModItemsInit.COFFEE_FILTER_USED.get());
-                    output.accept(ModItemsInit.ESPRESSO_SHOT.get());
-                    output.accept(ModItemsInit.STEAMED_MILK.get());
-                    output.accept(ModItemsInit.MILK_FOAM.get());
-                    output.accept(ModItemsInit.ICE_CUBES.get());
-
-                    output.accept(ModItemsInit.BOBA_PEARLS.get());
-                    output.accept(ModItemsInit.MATCHA_POWDER.get());
-
-                    // Dishes
-//                    output.accept(ModItemsInit.PAPER_CUP.get());
-                    output.accept(ModItemsInit.COFFEE_POT.get());
-                    output.accept(ModItemsInit.MUG.get());
-                    output.accept(ModItemsInit.CLEAR_CUP.get());
-
-                    output.accept(ModItemsInit.BATTER_MESS.get());
-                    output.accept(ModItemsInit.BURNT_CRISP.get());
-
-                    // Waffles
-                    output.accept(ModItemsInit.RAW_WAFFLE_BATTER.get());
-                    output.accept(ModItemsInit.TAIYAKI_MOLD.get());
-                    output.accept(ModItemsInit.EGG_WAFFLE_MOLD.get());
-                    output.accept(ModItemsInit.PAW_WAFFLE_MOLD.get());
-                    output.accept(ModItemsInit.HEART_WAFFLE_MOLD.get());
-                    output.accept(ModItemsInit.CLASSIC_WAFFLE_MOLD.get());
-                    output.accept(ModItemsInit.PUMPKIN_WAFFLE_MOLD.get());
-                    output.accept(ModItemsInit.MUSHROOM_WAFFLE_MOLD.get());
-                    output.accept(ModItemsInit.FLOWER_WAFFLE_MOLD.get());
-                    output.accept(ModItemsInit.CREEPER_WAFFLE_MOLD.get());
-                    output.accept(ModItemsInit.DIAMOND_WAFFLE_MOLD.get());
-                    output.accept(ModItemsInit.PICKAXE_WAFFLE_MOLD.get());
-                    output.accept(ModItemsInit.SWORD_WAFFLE_MOLD.get());
-                    output.accept(ModItemsInit.TAIYAKI.get());
-                    output.accept(ModItemsInit.EGG_WAFFLE.get());
-                    output.accept(ModItemsInit.PAW_WAFFLE.get());
-                    output.accept(ModItemsInit.HEART_WAFFLE.get());
-                    output.accept(ModItemsInit.CLASSIC_WAFFLE.get());
-                    output.accept(ModItemsInit.PUMPKIN_WAFFLE.get());
-                    output.accept(ModItemsInit.MUSHROOM_WAFFLE.get());
-                    output.accept(ModItemsInit.FLOWER_WAFFLE.get());
-                    output.accept(ModItemsInit.CREEPER_WAFFLE.get());
-                    output.accept(ModItemsInit.DIAMOND_WAFFLE.get());
-                    output.accept(ModItemsInit.PICKAXE_WAFFLE.get());
-                    output.accept(ModItemsInit.SWORD_WAFFLE.get());
-
-                    // Misc Food
-                    output.accept(ModItemsInit.CASSAVA_BIBINGKA.get());
-                    output.accept(ModItemsInit.TAPIOCA_PUDDING.get());
-                    /*
-                    Brownies
-                    Cannoli
-                    Cookies
-                    Croissants
-                    Whoopie pie
-                     */
-
-                    // Coffee Drinks
-                    output.accept(ModItemsInit.HOT_COCOA.get());
-                    output.accept(ModItemsInit.COFFEE.get());
-                    output.accept(ModItemsInit.BUTTER_COFFEE.get());
-                    output.accept(ModItemsInit.ESPRESSO.get());
-                    output.accept(ModItemsInit.RED_EYE_COFFEE.get());
-                    output.accept(ModItemsInit.BLACK_EYE_COFFEE.get());
-                    output.accept(ModItemsInit.DEAD_EYE_COFFEE.get());
-                    output.accept(ModItemsInit.AMERICANO.get());
-                    output.accept(ModItemsInit.MACCHIATO.get());
-                    output.accept(ModItemsInit.MOCHA.get());
-                    output.accept(ModItemsInit.CAFE_AU_LAIT.get());
-                    output.accept(ModItemsInit.FLAT_WHITE.get());
-                    output.accept(ModItemsInit.CAPPUCCINO.get());
-                    output.accept(ModItemsInit.LATTE.get());
-                    output.accept(ModItemsInit.FRAPPE_COFFEE.get());
-                    output.accept(ModItemsInit.WHIPPED_COFFEE.get());
-                    output.accept(ModItemsInit.ICED_COFFEE.get());
-
-                    // Teas
-                    output.accept(ModItemsInit.MATCHA_TEA.get());
-
-                    // Milk Teas
-                    output.accept(ModItemsInit.BROWN_SUGAR_MILK_TEA.get());
-                    output.accept(ModItemsInit.TIGER_MILK_TEA.get());
-                    output.accept(ModItemsInit.HONEY_MILK_TEA.get());
-                    output.accept(ModItemsInit.CHOCOLATE_MILK_TEA.get());
-                    output.accept(ModItemsInit.VANILLA_MILK_TEA.get());
-                    output.accept(ModItemsInit.EGG_CUSTARD_MILK_TEA.get());
-                    output.accept(ModItemsInit.COOKIE_MILK_TEA.get());
-                    output.accept(ModItemsInit.CAKE_MILK_TEA.get());
-                    output.accept(ModItemsInit.PUMPKIN_SPICE_MILK_TEA.get());
-                    output.accept(ModItemsInit.APPLE_MILK_TEA.get());
-                    output.accept(ModItemsInit.SWEET_BERRY_MILK_TEA.get());
-                    output.accept(ModItemsInit.WATERMELON_MILK_TEA.get());
-                    output.accept(ModItemsInit.GLOW_BERRY_MILK_TEA.get());
-                    output.accept(ModItemsInit.CHORUS_FRUIT_MILK_TEA.get());
-                    output.accept(ModItemsInit.ROSE_MILK_TEA.get());
-                    output.accept(ModItemsInit.BUTTERFLY_PEA_FLOWER_MILK_TEA.get());
-                    output.accept(ModItemsInit.MATCHA_MILK_TEA.get());
-                    output.accept(ModItemsInit.LAVENDER_MILK_TEA.get());
-                    output.accept(ModItemsInit.THAI_MILK_TEA.get());
-                    output.accept(ModItemsInit.BEETROOT_MILK_TEA.get());
-                    output.accept(ModItemsInit.CARROT_MILK_TEA.get());
-                    output.accept(ModItemsInit.KELP_MILK_TEA.get());
-                    output.accept(ModItemsInit.PHANTOM_MILK_TEA.get());
-                    output.accept(ModItemsInit.BLAZING_MILK_TEA.get());
-                    output.accept(ModItemsInit.DARKSIDE_MILK_TEA.get());
-                    output.accept(ModItemsInit.WET_MILK_TEA.get());
-                    output.accept(ModItemsInit.IMAGINARY_MILK_TEA.get());
-                })
-        );
-    }
-
-    private void addCreative(CreativeModeTabEvent.BuildContents event) {
-        if (event.getTab() == CreativeModeTabs.FOOD_AND_DRINKS) {
-            event.accept(ModItemsInit.CASSAVA);
-            event.accept(ModItemsInit.CASSAVA_BIBINGKA);
-            event.accept(ModItemsInit.TAPIOCA_PUDDING);
-
-            // Milk Teas
-            event.accept(ModItemsInit.BROWN_SUGAR_MILK_TEA);
-            event.accept(ModItemsInit.TIGER_MILK_TEA);
-            event.accept(ModItemsInit.HONEY_MILK_TEA);
-            event.accept(ModItemsInit.CHOCOLATE_MILK_TEA);
-            event.accept(ModItemsInit.VANILLA_MILK_TEA);
-            event.accept(ModItemsInit.EGG_CUSTARD_MILK_TEA);
-            event.accept(ModItemsInit.COOKIE_MILK_TEA);
-            event.accept(ModItemsInit.CAKE_MILK_TEA);
-            event.accept(ModItemsInit.PUMPKIN_SPICE_MILK_TEA);
-            event.accept(ModItemsInit.APPLE_MILK_TEA);
-            event.accept(ModItemsInit.SWEET_BERRY_MILK_TEA);
-            event.accept(ModItemsInit.WATERMELON_MILK_TEA);
-            event.accept(ModItemsInit.GLOW_BERRY_MILK_TEA);
-            event.accept(ModItemsInit.CHORUS_FRUIT_MILK_TEA);
-            event.accept(ModItemsInit.ROSE_MILK_TEA);
-            event.accept(ModItemsInit.BUTTERFLY_PEA_FLOWER_MILK_TEA);
-            event.accept(ModItemsInit.MATCHA_MILK_TEA);
-            event.accept(ModItemsInit.LAVENDER_MILK_TEA);
-            event.accept(ModItemsInit.THAI_MILK_TEA);
-            event.accept(ModItemsInit.BEETROOT_MILK_TEA);
-            event.accept(ModItemsInit.CARROT_MILK_TEA);
-            event.accept(ModItemsInit.KELP_MILK_TEA);
-            event.accept(ModItemsInit.PHANTOM_MILK_TEA);
-            event.accept(ModItemsInit.BLAZING_MILK_TEA);
-            event.accept(ModItemsInit.DARKSIDE_MILK_TEA);
-            event.accept(ModItemsInit.WET_MILK_TEA);
-            event.accept(ModItemsInit.IMAGINARY_MILK_TEA);
-
-            // Waffles
-            event.accept(ModItemsInit.RAW_WAFFLE_BATTER);
-            event.accept(ModItemsInit.TAIYAKI);
-            event.accept(ModItemsInit.EGG_WAFFLE);
-            event.accept(ModItemsInit.PAW_WAFFLE);
-            event.accept(ModItemsInit.HEART_WAFFLE);
-            event.accept(ModItemsInit.CLASSIC_WAFFLE);
-            event.accept(ModItemsInit.PUMPKIN_WAFFLE);
-            event.accept(ModItemsInit.MUSHROOM_WAFFLE);
-            event.accept(ModItemsInit.FLOWER_WAFFLE);
-            event.accept(ModItemsInit.CREEPER_WAFFLE);
-            event.accept(ModItemsInit.DIAMOND_WAFFLE);
-            event.accept(ModItemsInit.PICKAXE_WAFFLE);
-            event.accept(ModItemsInit.SWORD_WAFFLE);
-
-            event.accept(ModItemsInit.BATTER_MESS);
-            event.accept(ModItemsInit.BURNT_CRISP);
-
-            // Creams
-            event.accept(ModItemsInit.CREAM);
-            event.accept(ModItemsInit.WHIPPED_CREAM);
-            event.accept(ModItemsInit.CHOCOLATE_CREAM);
-            event.accept(ModItemsInit.VANILLA_CREAM);
-            event.accept(ModItemsInit.MATCHA_CREAM);
-            event.accept(ModItemsInit.HONEY_CREAM);
-            event.accept(ModItemsInit.ROSE_CREAM);
-            event.accept(ModItemsInit.MARSHMALLOW);
-            event.accept(ModItemsInit.MARSHMALLOWS);
-            event.accept(ModItemsInit.CUSTARD);
-
-            // Jams
-            event.accept(ModItemsInit.APPLE_JAM);
-            event.accept(ModItemsInit.CHORUS_FRUIT_JAM);
-            event.accept(ModItemsInit.GLOW_BERRY_JAM);
-            event.accept(ModItemsInit.SWEET_BERRY_JAM);
-            event.accept(ModItemsInit.WATERMELON_JAM);
-
-            // Coffee
-//            event.accept(ModItemsInit.COFFEE_CHERRIES);
-            event.accept(ModItemsInit.ESPRESSO);
-            event.accept(ModItemsInit.CAFE_AU_LAIT);
-            event.accept(ModItemsInit.AMERICANO);
-        }
-
-        if (event.getTab() == CreativeModeTabs.FUNCTIONAL_BLOCKS) {
-            event.accept(ModItemsInit.CAKE_STAND_ITEM);
-            event.accept(ModItemsInit.DISPLAY_CASE_CURVED_ITEM);
-            event.accept(ModItemsInit.OAK_WALL_SHELF_ITEM);
-            event.accept(ModItemsInit.WAFFLE_IRON_ITEM);
-            event.accept(ModItemsInit.COFFEE_MACHINE_ITEM);
-            event.accept(ModItemsInit.ESPRESSO_MACHINE_ITEM);
-        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/com/kmek/minecafe/item/ModCreativeTabInit.java
@@ -0,0 +1,213 @@
+package com.kmek.minecafe.item;
+
+import com.kmek.minecafe.MineCafeMod;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+
+public class ModCreativeTabInit {
+    public static CreativeModeTab bobaTab;
+
+    /**
+     * Registering the event bus
+     */
+    public void register(IEventBus modEventBus) {
+        modEventBus.addListener(this::registerCustomCreativeTab);
+    }
+
+    private void registerCustomCreativeTab(CreativeModeTabEvent.Register event) {
+        bobaTab = event.registerCreativeModeTab(new ResourceLocation(MineCafeMod.MODID, "minecafe"), builder -> builder
+            .title(Component.translatable("itemGroup.minecafe"))
+            .icon(() -> new ItemStack(ModItemsInit.BROWN_SUGAR_MILK_TEA.get()))
+            .displayItems((featureFlags, output, hasOp) -> {
+                // Blocks
+                acceptBlocks(output);
+
+                // Gardening / Some Ingredients
+                acceptCropsAndSeeds(output);
+
+                // Ingredients
+                acceptJams(output);
+                acceptCreams(output);
+                acceptCoffeeIngredients(output);
+                acceptMiscIngredients(output);
+
+                // Dishes
+                acceptDishes(output);
+
+                // Specialty Foods
+                acceptWaffles(output);
+                acceptMiscFoodItems(output);
+
+                // Specialty Drinks
+                acceptCoffeeDrinks(output);
+                acceptMilkTeaDrinks(output);
+            })
+        );
+    }
+
+    private void acceptBlocks(CreativeModeTab.Output output) {
+        output.accept(ModItemsInit.CAKE_STAND_ITEM.get());
+        output.accept(ModItemsInit.DISPLAY_CASE_CURVED_ITEM.get());
+        output.accept(ModItemsInit.OAK_WALL_SHELF_ITEM.get());
+        output.accept(ModItemsInit.WAFFLE_IRON_ITEM.get());
+        output.accept(ModItemsInit.COFFEE_MACHINE_ITEM.get());
+        output.accept(ModItemsInit.ESPRESSO_MACHINE_ITEM.get());
+
+        // Fun Blocks
+        output.accept(ModItemsInit.COFFEE_BUCKET.get());
+    }
+
+    private void acceptCropsAndSeeds(CreativeModeTab.Output output) {
+        output.accept(ModItemsInit.CASSAVA_CUTTING.get());
+        output.accept(ModItemsInit.CASSAVA.get());
+        output.accept(ModItemsInit.COFFEE_CHERRIES.get());
+    }
+
+    private void acceptJams(CreativeModeTab.Output output) {
+        output.accept(ModItemsInit.APPLE_JAM.get());
+        output.accept(ModItemsInit.CHORUS_FRUIT_JAM.get());
+        output.accept(ModItemsInit.GLOW_BERRY_JAM.get());
+        output.accept(ModItemsInit.SWEET_BERRY_JAM.get());
+        output.accept(ModItemsInit.WATERMELON_JAM.get());
+    }
+
+    private void acceptCreams(CreativeModeTab.Output output) {
+        output.accept(ModItemsInit.CREAM.get());
+        output.accept(ModItemsInit.WHIPPED_CREAM.get());
+        output.accept(ModItemsInit.CHOCOLATE_CREAM.get());
+        output.accept(ModItemsInit.VANILLA_CREAM.get());
+        output.accept(ModItemsInit.MATCHA_CREAM.get());
+        output.accept(ModItemsInit.HONEY_CREAM.get());
+        output.accept(ModItemsInit.ROSE_CREAM.get());
+        output.accept(ModItemsInit.MARSHMALLOW.get());
+        output.accept(ModItemsInit.MARSHMALLOWS.get());
+        output.accept(ModItemsInit.CUSTARD.get());
+        output.accept(ModItemsInit.BUTTER.get());
+
+        output.accept(ModItemsInit.CHOCOLATE.get());
+    }
+
+    private void acceptMiscIngredients(CreativeModeTab.Output output) {
+        output.accept(ModItemsInit.BOBA_PEARLS.get());
+        output.accept(ModItemsInit.MATCHA_POWDER.get());
+        output.accept(ModItemsInit.RAW_WAFFLE_BATTER.get());
+        output.accept(ModItemsInit.BATTER_MESS.get());
+        output.accept(ModItemsInit.BURNT_CRISP.get());
+    }
+
+    private void acceptDishes(CreativeModeTab.Output output) {
+        output.accept(ModItemsInit.COFFEE_POT.get());
+        output.accept(ModItemsInit.MUG.get());
+        output.accept(ModItemsInit.CLEAR_CUP.get());
+        // output.accept(ModItemsInit.PAPER_CUP.get());
+        // output.accept(ModItemsInit.PASTRY_BAG.get());
+        output.accept(ModItemsInit.TAIYAKI_MOLD.get());
+        output.accept(ModItemsInit.EGG_WAFFLE_MOLD.get());
+        output.accept(ModItemsInit.PAW_WAFFLE_MOLD.get());
+        output.accept(ModItemsInit.HEART_WAFFLE_MOLD.get());
+        output.accept(ModItemsInit.CLASSIC_WAFFLE_MOLD.get());
+        output.accept(ModItemsInit.PUMPKIN_WAFFLE_MOLD.get());
+        output.accept(ModItemsInit.MUSHROOM_WAFFLE_MOLD.get());
+        output.accept(ModItemsInit.FLOWER_WAFFLE_MOLD.get());
+        output.accept(ModItemsInit.CREEPER_WAFFLE_MOLD.get());
+        output.accept(ModItemsInit.DIAMOND_WAFFLE_MOLD.get());
+        output.accept(ModItemsInit.PICKAXE_WAFFLE_MOLD.get());
+        output.accept(ModItemsInit.SWORD_WAFFLE_MOLD.get());
+    }
+
+    private void acceptWaffles(CreativeModeTab.Output output) {
+        output.accept(ModItemsInit.TAIYAKI.get());
+        output.accept(ModItemsInit.EGG_WAFFLE.get());
+        output.accept(ModItemsInit.PAW_WAFFLE.get());
+        output.accept(ModItemsInit.HEART_WAFFLE.get());
+        output.accept(ModItemsInit.CLASSIC_WAFFLE.get());
+        output.accept(ModItemsInit.PUMPKIN_WAFFLE.get());
+        output.accept(ModItemsInit.MUSHROOM_WAFFLE.get());
+        output.accept(ModItemsInit.FLOWER_WAFFLE.get());
+        output.accept(ModItemsInit.CREEPER_WAFFLE.get());
+        output.accept(ModItemsInit.DIAMOND_WAFFLE.get());
+        output.accept(ModItemsInit.PICKAXE_WAFFLE.get());
+        output.accept(ModItemsInit.SWORD_WAFFLE.get());
+    }
+
+    private void acceptMiscFoodItems(CreativeModeTab.Output output) {
+        output.accept(ModItemsInit.CASSAVA_BIBINGKA.get());
+        output.accept(ModItemsInit.TAPIOCA_PUDDING.get());
+        /*
+        Brownies
+        Cannoli
+        Cookies
+        Croissants
+        Whoopie pie
+         */
+    }
+
+    private void acceptMilkTeaDrinks(CreativeModeTab.Output output) {
+        output.accept(ModItemsInit.MATCHA_TEA.get());
+
+        output.accept(ModItemsInit.BROWN_SUGAR_MILK_TEA.get());
+        output.accept(ModItemsInit.TIGER_MILK_TEA.get());
+        output.accept(ModItemsInit.HONEY_MILK_TEA.get());
+        output.accept(ModItemsInit.CHOCOLATE_MILK_TEA.get());
+        output.accept(ModItemsInit.VANILLA_MILK_TEA.get());
+        output.accept(ModItemsInit.EGG_CUSTARD_MILK_TEA.get());
+        output.accept(ModItemsInit.COOKIE_MILK_TEA.get());
+        output.accept(ModItemsInit.CAKE_MILK_TEA.get());
+        output.accept(ModItemsInit.PUMPKIN_SPICE_MILK_TEA.get());
+        output.accept(ModItemsInit.APPLE_MILK_TEA.get());
+        output.accept(ModItemsInit.SWEET_BERRY_MILK_TEA.get());
+        output.accept(ModItemsInit.WATERMELON_MILK_TEA.get());
+        output.accept(ModItemsInit.GLOW_BERRY_MILK_TEA.get());
+        output.accept(ModItemsInit.CHORUS_FRUIT_MILK_TEA.get());
+        output.accept(ModItemsInit.ROSE_MILK_TEA.get());
+        output.accept(ModItemsInit.BUTTERFLY_PEA_FLOWER_MILK_TEA.get());
+        output.accept(ModItemsInit.MATCHA_MILK_TEA.get());
+        output.accept(ModItemsInit.LAVENDER_MILK_TEA.get());
+        output.accept(ModItemsInit.THAI_MILK_TEA.get());
+        output.accept(ModItemsInit.BEETROOT_MILK_TEA.get());
+        output.accept(ModItemsInit.CARROT_MILK_TEA.get());
+        output.accept(ModItemsInit.KELP_MILK_TEA.get());
+        output.accept(ModItemsInit.PHANTOM_MILK_TEA.get());
+        output.accept(ModItemsInit.BLAZING_MILK_TEA.get());
+        output.accept(ModItemsInit.DARKSIDE_MILK_TEA.get());
+        output.accept(ModItemsInit.WET_MILK_TEA.get());
+        output.accept(ModItemsInit.IMAGINARY_MILK_TEA.get());
+    }
+
+    private void acceptCoffeeIngredients(CreativeModeTab.Output output) {
+        output.accept(ModItemsInit.COFFEE_BEANS_UNROASTED.get());
+        output.accept(ModItemsInit.COFFEE_BEANS_ROASTED.get());
+        output.accept(ModItemsInit.COFFEE_GROUNDS.get());
+        output.accept(ModItemsInit.COFFEE_POT_FULL.get());
+        output.accept(ModItemsInit.COFFEE_FILTER.get());
+        output.accept(ModItemsInit.COFFEE_FILTER_USED.get());
+        output.accept(ModItemsInit.ESPRESSO_SHOT.get());
+        output.accept(ModItemsInit.STEAMED_MILK.get());
+        output.accept(ModItemsInit.MILK_FOAM.get());
+        output.accept(ModItemsInit.ICE_CUBES.get());
+    }
+
+    private void acceptCoffeeDrinks(CreativeModeTab.Output output) {
+        output.accept(ModItemsInit.HOT_COCOA.get());
+        output.accept(ModItemsInit.COFFEE.get());
+        output.accept(ModItemsInit.BUTTER_COFFEE.get());
+        output.accept(ModItemsInit.ESPRESSO.get());
+        output.accept(ModItemsInit.RED_EYE_COFFEE.get());
+        output.accept(ModItemsInit.BLACK_EYE_COFFEE.get());
+        output.accept(ModItemsInit.DEAD_EYE_COFFEE.get());
+        output.accept(ModItemsInit.AMERICANO.get());
+        output.accept(ModItemsInit.MACCHIATO.get());
+        output.accept(ModItemsInit.MOCHA.get());
+        output.accept(ModItemsInit.CAFE_AU_LAIT.get());
+        output.accept(ModItemsInit.FLAT_WHITE.get());
+        output.accept(ModItemsInit.CAPPUCCINO.get());
+        output.accept(ModItemsInit.LATTE.get());
+        output.accept(ModItemsInit.FRAPPE_COFFEE.get());
+        output.accept(ModItemsInit.WHIPPED_COFFEE.get());
+        output.accept(ModItemsInit.ICED_COFFEE.get());
+    }
+}
