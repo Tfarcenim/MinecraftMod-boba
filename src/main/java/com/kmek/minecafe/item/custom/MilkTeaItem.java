4aeef822b12bb1c953ca9bcb105399bee9d69292
Kmek <kmekeefe@gmail.com>
2023-02-11 20:37:07 -0500

    Animate coffee textures to have steam

--- src/main/java/com/kmek/minecafe/item/custom/MilkTeaItem.java
+++ src/main/java/com/kmek/minecafe/item/custom/MilkTeaItem.java
@@ -1,9 +1,13 @@
 package com.kmek.minecafe.item.custom;
 
 import com.kmek.minecafe.item.ModItemsInit;
-import com.kmek.minecafe.item.custom.CustomDrinkItem;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
 
 public class MilkTeaItem extends CustomDrinkItem {
     public MilkTeaItem(Item.Properties properties) {
@@ -26,5 +30,10 @@ public class MilkTeaItem extends CustomDrinkItem {
         this.toReturnAfterUse = ModItemsInit.CLEAR_CUP.get();
     }
 
-    // todo future idea: cure all harmful potions the same way milk does
+    // Cure all harmful potions the same way milk does
+    @Override
+    public @NotNull ItemStack finishUsingItem(@NotNull ItemStack itemStack, @NotNull Level level, @NotNull LivingEntity livingEntity) {
+        livingEntity.curePotionEffects(new ItemStack(Items.MILK_BUCKET));
+        return super.finishUsingItem(itemStack, level, livingEntity);
+    }
 }
