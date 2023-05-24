8cea11ccc8e341755ed751cbb533593fac8223f3
Kmek <kmekeefe@gmail.com>
2023-05-23 21:59:06 -0400

    Add datagen for cake blockstates

--- src/main/java/com/kmek/minecafe/datagen/ModBlockStateProvider.java
+++ src/main/java/com/kmek/minecafe/datagen/ModBlockStateProvider.java
@@ -1,9 +1,12 @@
 package com.kmek.minecafe.datagen;
 
 import com.kmek.minecafe.MineCafeMod;
+import com.kmek.minecafe.block.ModBlocksInit;
 import net.minecraft.data.PackOutput;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.client.model.generators.ConfiguredModel;
 import net.minecraftforge.client.model.generators.VariantBlockStateBuilder;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.registries.RegistryObject;
@@ -15,10 +18,23 @@ public class ModBlockStateProvider extends BlockStateProvider {
 
     @Override
     protected void registerStatesAndModels() {
-
+        cakeBlockState(ModBlocksInit.CARROT_CAKE_BLOCK);
     }
 
-//    private VariantBlockStateBuilder yDirectionalBlockStates(RegistryObject<Block> block) {
-//        return new VariantBlockStateBuilder();
-//    }
+    private VariantBlockStateBuilder cakeBlockState(RegistryObject<Block> block) {
+        VariantBlockStateBuilder builder = getVariantBuilder(block.get());
+        return builder.forAllStates(state -> {
+            int bites = state.getValue(BlockStateProperties.BITES);
+            String modelName = "block/cake/" + block.getId().getPath(); // block/cake/chocolate_cake
+            if (bites > 0) { modelName += "_slice" + bites; }
+            return ConfiguredModel.builder()
+                .modelFile(models().getExistingFile(modLoc(modelName)))
+                .build();
+        });
+//        return builder.partialState()
+//            .with(BlockStateProperties.BITES, 0)
+//                .modelForState()
+//                .modelFile(models().getExistingFile(modLoc("block/cake/" + block.getId().getPath())))
+//                .addModel();
+    }
 }
